17d1bb92a2ac6e71ecad9421b9bfaaf38272c122
dabiiXqx <khaledroomi2013@gmail.com>
2026-02-14 07:40:10 +0000

    Declaring kernel32.c is incomplete
    Thinking about implementing the IDT later on and repairing it to stop bugs

--- KernelFiles/kernel/CFiles/kernel32.c
+++ KernelFiles/kernel/CFiles/kernel32.c
@@ -1,4 +1,7 @@
 //i swear im not copying windows
+
+
+//obviously incomplete, implementing IDT and attempting repairs later on.
 #include "../HeaderFiles/kernel32.h"
 void kernel_main(){
 	WriteToVGA("Kernel :D", 0xf, 0x1, 1);
